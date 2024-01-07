539d000d2135b729c2bfdbd836089756816a8ec6
MohYusrilprayoga <124210006@student.upnyk.ac.id>
2024-01-07 11:10:10 +0700

    Update Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="flex max-md:flex-col flex-wrap justify-between px-6 py-10 gap-5 sm:px-16">
         <div className="flex justify-start items-start flex-col gap-6">
           <Image
-            src="/logo.svg"
+            src="/yusrilprayoga.svg"
             alt="logo"
             width={118}
             height={18}
